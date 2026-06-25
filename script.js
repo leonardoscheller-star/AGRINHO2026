b595484d7b374a94a8ee43cf30906bd9e57ba398
leonardoscheller-star <leonardo.scheller@escola.pr.gov.br>
2026-06-25 20:31:04 -0300

    salv

---
+++ script.js
@@ -0,0 +1,21 @@
+// Começamos dizendo que o tamanho padrão é 16
+let tamanhoAtual = 16; 
+
+// Função que aumenta ou diminui o texto
+function mudarTamanho(mudanca) {
+    // Adiciona ou subtrai 2 pontos no tamanho da fonte
+    tamanhoAtual = tamanhoAtual + (mudanca * 2); 
+    
+    // Travas de segurança para a letra não sumir ou ficar gigante demais
+    if (tamanhoAtual < 12) tamanhoAtual = 12;
+    if (tamanhoAtual > 32) tamanhoAtual = 32;
+
+    // Aplica o novo tamanho no site inteiro
+    document.body.style.fontSize = tamanhoAtual + "px";
+}
+
+// Função que faz o texto voltar ao normal
+function resetarTamanho() {
+    tamanhoAtual = 16; // Volta para o 16
+    document.body.style.fontSize = tamanhoAtual + "px"; // Aplica
+}
