af6f4bc4f945c3abd0b869478e68619452c4529a
leonardoscheller-star <leonardo.scheller@escola.pr.gov.br>
2026-06-25 20:58:04 -0300

    salva

--- script.js
+++ script.js
@@ -8,3 +8,21 @@ function mudarTamanho(mudanca) {
 
     document.body.style.fontSize = tamanhoAtual + "px";
 }
+
+function salvarNome() {
+    // 1. O Javascript vai lá no HTML e pega o que está escrito dentro do <input>
+    let nomeDigitado = document.getElementById("nome-usuario").value;
+    if (nomeDigitado !== "") {
+        
+        // 3. Esconde a caixinha de pergunta (Display "none" significa sumir)
+        document.getElementById("area-pergunta").style.display = "none";
+        
+        // 4. Pega a nossa tag <h2> vazia e escreve uma mensagem dentro dela juntando com o nome
+        let mensagem = document.getElementById("mensagem-saudacao");
+        mensagem.innerHTML = "Olá, " + nomeDigitado + "! Que bom ter você aqui aprendendo sobre o Agro.";
+        
+        // 5. Faz a mensagem aparecer na tela (Display "block" faz aparecer)
+        mensagem.style.display = "block";
+
+    } 
+}
